bcc538acac6cdbd263d60d65559e89d811ed40bb
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-05-21 17:56:56 -0300

    created credentials component

---
+++ src/components/perfil/credentials/index.tsx
@@ -0,0 +1,14 @@
+import StyleCredentials from './Credentials.module.scss';
+
+export default function Credentials() {
+    return (
+        <div>
+            <div className={StyleCredentials.title}>
+                Guilherme Amorim
+            </div>
+            <div className={StyleCredentials.sub_title}>
+                Full Stack
+            </div>
+        </div>
+    )
+}
--- src/components/perfil/index.tsx
+++ src/components/perfil/index.tsx
@@ -1,10 +1,12 @@
 import StylePerfil from './Perfil.module.scss';
+import Credentials from './credentials';
 import Photo from './photo';
 
 export default function Perfil() {
     return (
         <div className={StylePerfil.perfil}>
             <Photo />
+            <Credentials />
         </div>
     )
 }
