958f7d6d2473740c5073fd3953c040b3f503982d
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-05-22 17:30:15 -0300

    feat: add space-around to Certificate

--- src/components/content/certifications/certificate/index.tsx
+++ src/components/content/certifications/certificate/index.tsx
@@ -1,4 +1,4 @@
-import StyleCertifications from "./Certifications.module.scss";
+import StyleCertificate from "./Certificate.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { ExternalLink } from "react-external-link";
@@ -22,30 +22,24 @@ export default function Certificate(props: IProps) {
         certificateExternalLink,
     } = props;
     return (
-        <div>
-            <div className={StyleCertifications.card}>
-                <img
-                    src={imgSrc}
-                    alt={imgAlt}
-                    className={StyleCertifications.photo}
-                />
-                <div className={StyleCertifications.info}>
-                    <div>
-                        <h2>{certificateTitle}</h2>
-                        <p>{certificateInstitution}</p>
-                        <p>{certificateIssueDate}</p>
-                    </div>
+        <div className={StyleCertificate.card}>
+            <img src={imgSrc} alt={imgAlt} className={StyleCertificate.photo} />
+            <div className={StyleCertificate.info}>
+                <div>
+                    <h2>{certificateTitle}</h2>
+                    <p>{certificateInstitution}</p>
+                    <p>{certificateIssueDate}</p>
                 </div>
-                <ExternalLink
-                    className={StyleCertifications.link}
-                    href={certificateExternalLink}
-                >
-                    <FontAwesomeIcon
-                        icon={faEye}
-                        className={StyleCertifications.link_icon}
-                    />
-                </ExternalLink>
             </div>
+            <ExternalLink
+                className={StyleCertificate.link}
+                href={certificateExternalLink}
+            >
+                <FontAwesomeIcon
+                    icon={faEye}
+                    className={StyleCertificate.link_icon}
+                />
+            </ExternalLink>
         </div>
     );
 }
--- src/components/content/certifications/index.tsx
+++ src/components/content/certifications/index.tsx
@@ -1,11 +1,60 @@
+import Certificate from "./certificate";
 import StyleCertifications from "./Certifications.module.scss";
 
 export default function Certifications() {
+    const certifications = [
+        {
+            id: 1,
+            imgSrc: "https://media-exp1.licdn.com/dms/image/C4E0BAQEHX7cfhFZv8w/company-logo_100_100/0/1638241687335?e=1661385600&v=beta&t=bbAcJg6oR4UlHEkIjUkwlMn9NFowLsgPzYSjlLnJzNI",
+            imgAlt: "Alura",
+            certificateTitle: "React: conhecendo a biblioteca React Router",
+            certificateInstitution: "Alura",
+            certificateIssueDate:
+                "Emitido em mai de 2022 · Sem data de expiração",
+            certificateExternalLink:
+                "https://cursos.alura.com.br/certificate/debea904-b285-4566-a1c1-34acae617409",
+        },
+        {
+            id: 2,
+            imgSrc: "https://media-exp1.licdn.com/dms/image/C4E0BAQEHX7cfhFZv8w/company-logo_100_100/0/1638241687335?e=1661385600&v=beta&t=bbAcJg6oR4UlHEkIjUkwlMn9NFowLsgPzYSjlLnJzNI",
+            imgAlt: "Alura",
+            certificateTitle: "React: conhecendo a biblioteca React Router",
+            certificateInstitution: "Alura",
+            certificateIssueDate:
+                "Emitido em mai de 2022 · Sem data de expiração",
+            certificateExternalLink:
+                "https://cursos.alura.com.br/certificate/debea904-b285-4566-a1c1-34acae617409",
+        },
+        {
+            id: 3,
+            imgSrc: "https://media-exp1.licdn.com/dms/image/C4E0BAQEHX7cfhFZv8w/company-logo_100_100/0/1638241687335?e=1661385600&v=beta&t=bbAcJg6oR4UlHEkIjUkwlMn9NFowLsgPzYSjlLnJzNI",
+            imgAlt: "Alura",
+            certificateTitle: "React: conhecendo a biblioteca React Router",
+            certificateInstitution: "Alura",
+            certificateIssueDate:
+                "Emitido em mai de 2022 · Sem data de expiração",
+            certificateExternalLink:
+                "https://cursos.alura.com.br/certificate/debea904-b285-4566-a1c1-34acae617409",
+        },
+        {
+            id: 3,
+            imgSrc: "https://media-exp1.licdn.com/dms/image/C4E0BAQEHX7cfhFZv8w/company-logo_100_100/0/1638241687335?e=1661385600&v=beta&t=bbAcJg6oR4UlHEkIjUkwlMn9NFowLsgPzYSjlLnJzNI",
+            imgAlt: "Alura",
+            certificateTitle: "React: conhecendo a biblioteca React Router",
+            certificateInstitution: "Alura",
+            certificateIssueDate:
+                "Emitido em mai de 2022 · Sem data de expiração",
+            certificateExternalLink:
+                "https://cursos.alura.com.br/certificate/debea904-b285-4566-a1c1-34acae617409",
+        },
+    ];
     return (
         <div className={StyleCertifications.certifications}>
             <h1>Licenças e certificados</h1>
             <div className={StyleCertifications.certifications_box}>
-
+                {certifications.map((certificate) => (
+                    <Certificate key={certificate.id} {...certificate} />
+                ))}
             </div>
         </div>
     );
